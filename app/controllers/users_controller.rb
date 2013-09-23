effe5c800ed05bf82fdb43f18accbc9d5d038144
Jason Hamm <jhamm.business@gmail.com>
2013-09-23 06:13:17 -0400

    Add ability to destroy users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
-def index
+  def index
     @users = User.paginate(page: params[:page])
   end
 
@@ -37,6 +38,12 @@ def index
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   private
 
   def user_params
@@ -56,5 +63,9 @@ def index
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
