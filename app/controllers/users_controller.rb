f408bc2d715060674acd939c4055b828776a7861
Jason Hamm <jhamm.business@gmail.com>
2013-09-23 05:36:18 -0400

    Update profile - 9.1.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
